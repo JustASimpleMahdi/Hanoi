c98ff4b35070aff669f73800a765837acf13cdaf
mahdi <just.a.simple.mahdi@gmail.com>
2022-04-30 19:24:47 +0430

    The Algorithm

---
+++ script.js
@@ -0,0 +1,17 @@
+hanoi(3, 1, 3)
+
+
+function hanoi(n, start, end) {
+    if (n === 1) move_one(start, end)
+    else {
+        // This line is very interesting
+        let other = 6 - (start + end)
+
+        hanoi(n - 1, start, other)
+        move_one(start, end)
+        hanoi(n - 1, other, end)
+    }
+}
+function move_one(start, end) {
+    console.log(`${start} -> ${end}`)
+}
